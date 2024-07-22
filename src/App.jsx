1135e044df56f05092a5843e18377eba8a05ece5
cristhian Villamarin <cristhianvillamarin2002@gmail.com>
2024-07-22 12:19:50 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect } from 'react';
+import {
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+
+import './css/style.css';
+
+import './charts/ChartjsConfig';
+
+// Import pages
+import UsersTabs from './pages/community/UsersTabs';
+import UsersTiles from './pages/community/UsersTiles';
+import Profile from './pages/community/Profile';
+import Feed from './pages/community/Feed';
+import Forum from './pages/community/Forum';
+import ForumPost from './pages/community/ForumPost';
+import Meetups from './pages/community/Meetups';
+import MeetupsPost from './pages/community/MeetupsPost';
+import JobListing from './pages/job/JobListing';
+import JobPost from './pages/job/JobPost';
+import CompanyProfile from './pages/job/CompanyProfile';
+import Messages from './pages/Messages';
+import Inbox from './pages/Inbox';
+import Calendar from './pages/Calendar';
+import Account from './pages/settings/Account';
+import Notifications from './pages/settings/Notifications';
+import Apps from './pages/settings/Apps';
+import Plans from './pages/settings/Plans';
+import Billing from './pages/settings/Billing';
+import Feedback from './pages/settings/Feedback';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import ResetPassword from './pages/ResetPassword';
+import Onboarding01 from './pages/Onboarding01';
+import Onboarding02 from './pages/Onboarding02';
+import Onboarding03 from './pages/Onboarding03';
+import Onboarding04 from './pages/Onboarding04';
+import PageNotFound from './pages/utility/PageNotFound';
+
+
+function App() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    document.querySelector('html').style.scrollBehavior = 'auto'
+    window.scroll({ top: 0 })
+    document.querySelector('html').style.scrollBehavior = ''
+  }, [location.pathname]); // triggered on route change
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<UsersTabs />} />
+        <Route path="/community/users-tabs" element={<UsersTabs />} />
+        <Route path="/community/users-tiles" element={<UsersTiles />} />
+        <Route path="/community/profile" element={<Profile />} />
+        <Route path="/community/feed" element={<Feed />} />
+        <Route path="/community/forum" element={<Forum />} />
+        <Route path="/community/forum-post" element={<ForumPost />} />
+        <Route path="/community/meetups" element={<Meetups />} />
+        <Route path="/community/meetups-post" element={<MeetupsPost />} />
+        <Route path="/job/job-listing" element={<JobListing />} />
+        <Route path="/job/job-post" element={<JobPost />} />
+        <Route path="/job/company-profile" element={<CompanyProfile />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/inbox" element={<Inbox />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/settings/account" element={<Account />} />
+        <Route path="/settings/notifications" element={<Notifications />} />
+        <Route path="/settings/apps" element={<Apps />} />
+        <Route path="/settings/plans" element={<Plans />} />
+        <Route path="/settings/billing" element={<Billing />} />
+        <Route path="/settings/feedback" element={<Feedback />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/onboarding-01" element={<Onboarding01 />} />
+        <Route path="/onboarding-02" element={<Onboarding02 />} />
+        <Route path="/onboarding-03" element={<Onboarding03 />} />
+        <Route path="/onboarding-04" element={<Onboarding04 />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
